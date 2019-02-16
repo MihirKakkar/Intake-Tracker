799dab47486faa9cbd2af1d6eaba79d47894627d
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2019-02-16 17:27:41 -0500

    Add files via upload

---
+++ Intake Tracker.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 16:18:57 2019
+
+@author: mihir
+"""
+
+import tkinter as tk
+
+#original base strings to add one to
+watercounter = ""
+mealcounter = ""
+
+
+
+#random letters for length counter
+n = "n"
+k = "k"
+
+# trying arrays instead
+
+waterlist = list()
+meallist = list()
+
+#Keeps water count
+def add_to_watercounter(watercounter):
+    waterlist.append(1)
+    print("Glasses of water had today: ")
+    x = len(waterlist)
+    print(x)
+
+#Keeps meal count 
+def add_to_mealcounter(mealcounter):
+    meallist.append(1)
+    print("Meals had today: ")
+    y = len(meallist)
+    print(y)
+    
+    
+#Root-title
+root = tk.Tk()
+root.title("Water Counter")
+label = tk.Label(root, fg = "red")
+label.pack()
+
+#defining button 1
+button1 = tk.Button(root, width = 50, text = "I just had a glass of water", bg="blue", command=lambda: add_to_watercounter(watercounter))
+button1.pack()
+
+#defining button 2
+button2 = tk.Button(root, width = 50, text = "I just had a meal", bg="red", command=lambda: add_to_mealcounter(mealcounter))
+button2.pack()
+
+
+root.mainloop()
